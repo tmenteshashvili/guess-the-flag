451c4e0897515bed419a5f7f0e9e8fbfc5c00092
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2022-09-30 15:32:35 +0400

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Tako Menteshashvili on 30.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Tako Menteshashvili on 30.09.22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
